f5e1a2cac2436a70838a0302fc66165d3de8fc65
Mesut Çtr <mesut.cittir@bil.omu.edu.tr>
2017-12-04 00:17:49 +0300

    google analytics code appended

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -13,6 +13,16 @@
 <link rel="stylesheet" href="{{asset('acron/assets/css/master.css')}}">
 <link rel="stylesheet" href="{{asset('acron/assets/css/responsive.css')}}">
 <link rel="stylesheet" href="{{asset('css/style.css')}}">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110579981-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-110579981-1');
+</script>
+
 </head>
 <body>
 
